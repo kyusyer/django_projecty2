c0d2cebfa7a640411422c8edeaf5dcfd537fbb46
Alvin <kyusyer@gmail,com>
2024-02-09 01:04:53 +0800

    modifies project.html - add loop to avoid hard coding each task. modifies add.html to enable adding of project. used task form for input

--- alpha/my_profile/urls.py
+++ alpha/my_profile/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "my_profile"
 urlpatterns = [
     path("",views.index, name="index"),
-    path("project", views.project, name="project")
+    path("project", views.project, name="project"),
+    path("add",views.add_project, name="add")
 ]
--- alpha/my_profile/views.py
+++ alpha/my_profile/views.py
@@ -1,10 +1,54 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django import forms
+
+
+class NewTaskForm(forms.Form):
+    name = forms.CharField(label="New Project")
+    desc = forms.CharField(label="Description")
+    link = forms.CharField(label="Link")
+
+    
+projects = [
+    {
+        "name": "Morse Code:Encoder Decoder",
+        "desc": "A program written in python that converts alphaneumeric characters into morse code\
+              or convert morse code into alphanumeric characters",
+        "link": "https://github.com/kyusyer/morsecode"
+    },
+    {
+        "name": "Fullstack website using django",
+        "desc": "A full stack website that contains my resume that is written in django",
+        "link": "https://github.com/kyusyer/django_projecty2"
+    }
+]
+
+
 # Create your views here.
 
 def index(request):
     return render(request,"my_profile/index.html")
 
 def project(request):
-    return render(request, "my_profile/project.html")
+    return render(request, "my_profile/project.html", {
+        "projects": projects
+    })
+
+
+def add_project(request): 
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            desc = form.cleaned_data["desc"]
+            link = form.cleaned_data["link"]
+            projects.append(
+                {
+                    "name": name,
+                    "desc": desc,
+                    "link": link
+                }
+            )
+
+    return render(request, "my_profile/add_project.html", {"form": NewTaskForm()} )
