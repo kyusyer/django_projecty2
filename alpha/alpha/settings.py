1eb21497465fff3b9449a1d551e5286be2882274
alvin garin <123142409+kyusyer@users.noreply.github.com>
2024-02-09 19:36:04 +0800

    Update settings.py
    debug - True to debug

--- alpha/alpha/settings.py
+++ alpha/alpha/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!capcr7x*gc6)9ims-_g+x3zjt9bemyzylw!bgcts%ikl4vw^u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'djangoprojecty2-production.up.railway.app'
