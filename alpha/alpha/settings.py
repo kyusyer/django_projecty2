703bee03963420758cdf001225b27e571e188ac4
alvin garin <123142409+kyusyer@users.noreply.github.com>
2024-02-09 19:27:44 +0800

    Update settings.py
    added allowed host

--- alpha/alpha/settings.py
+++ alpha/alpha/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-!capcr7x*gc6)9ims-_g+x3zjt9bemyzylw!bgcts%ikl4vw^u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'djangoprojecty2-production.up.railway.app'
+]
 
 
 # Application definition
