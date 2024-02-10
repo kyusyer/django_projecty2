a690853f70de361ef86984b80c24d050bb9a4894
Alvin <kyusyer@gmail,com>
2024-02-10 21:22:35 +0800

    update settings.py. Added CSR_TRUSTED_ORIGINS at line 58

--- alpha/alpha/settings.py
+++ alpha/alpha/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
+CSRF_TRUSTED_ORIGINS = ['https://djangoprojecty2-production.up.railway.app/']
 
 ROOT_URLCONF = 'alpha.urls'
 
